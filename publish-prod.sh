ff80b730dd153fafe7a94c6775b0867dd3a7abaa
Aaron Faanes <dafrito@gmail.com>
2022-02-21 14:29:45 -0600

    upgrade-microproject

--- publish-prod.sh
+++ publish-prod.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PUBLISH="npm pub parsegraph-gettimeinmillis-dev.tgz --tag=dev"
+PUBLISH="npm pub parsegraph-gettimeinmillis-prod.tgz"
 $PUBLISH && exit
 for try in `seq 1 3`; do
     sleep $try
